e1379ea6776809921e2a1d47956fed9fd8e5377d
Anh Viet Pham <pham.anh.viet.vlth@gmail.com>
2019-05-27 22:59:25 +0700

    Add HomeSearchTrendMapper.kt

--- app/src/main/java/com/avp/mvvm_tesing/data/repository/home/mapper/HomeSearchTrendMapper.kt
+++ app/src/main/java/com/avp/mvvm_tesing/data/repository/home/mapper/HomeSearchTrendMapper.kt
@@ -1,12 +1,29 @@
 package com.avp.mvvm_tesing.data.repository.home.mapper
 
 import com.avp.mvvm_tesing.data.api.response.home.searchtrend.HomeSearchTrendResponse
+import com.avp.mvvm_tesing.domain.usecase.features.home.searchtrend.result.HomeSearchTrendDataResultModel
 import com.avp.mvvm_tesing.domain.usecase.features.home.searchtrend.result.HomeSearchTrendResultModel
+import com.avp.mvvm_tesing.utils.extension.valueOrEmpty
+import com.avp.mvvm_tesing.utils.extension.valueOrZero
 import com.avp.mvvm_tesing.utils.mapper.Mapper
 import javax.inject.Inject
 
 class HomeSearchTrendMapper @Inject constructor() : Mapper<HomeSearchTrendResponse, HomeSearchTrendResultModel>() {
     override fun map(input: HomeSearchTrendResponse): HomeSearchTrendResultModel {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val homeSearchTrendDataResultModels = mutableListOf<HomeSearchTrendDataResultModel>()
+        input.data?.forEach { item ->
+            item?.let { it ->
+                val homeSearchTrendDataResultModel = HomeSearchTrendDataResultModel(
+                    deeplink = it.deeplink.valueOrEmpty(),
+                    name = it.name.valueOrEmpty(),
+                    image = it.image.valueOrEmpty(),
+                    link = it.link.valueOrEmpty(),
+                    q = it.q.valueOrEmpty(),
+                    totalCount = it.totalCount.valueOrZero()
+                )
+                homeSearchTrendDataResultModels.add(homeSearchTrendDataResultModel)
+            }
+        }
+        return HomeSearchTrendResultModel(data = homeSearchTrendDataResultModels)
     }
 }
